8a79eb9c59340a8f857da8e565a6b3d6086888e1
Michael Goettsche <michael@michael-goettsche.de>
2015-10-09 08:52:52 +0200

    Add Item 2: Follow the PEP 8 Style Guide

---
+++ item2.py
@@ -0,0 +1,16 @@
+# Item 2: Follow the PEP 8 Style Guide
+
+# Most important rules from PEP 8.
+# Selection:
+# Continuation of long expressions should be intended by another 4 spaces from normal indentation level.
+#
+# lowercase_underscore naming for functions, variables and attributes.
+# _leading_underscore for protected instance attributes.
+# __double_leading_underscore for private instance attributes.
+# CapitalizationLikeThis for classes and exceptions.
+#
+# Check for empty values like 'if not somelist' instead of 'if len(somelist) == 0'.
+# Conversely, Check for non-empty values like 'if somelist'.
+# Imports in order: standard library, third party, own modules.
+#
+# Use Pylint to check compliance with PEP 8.
